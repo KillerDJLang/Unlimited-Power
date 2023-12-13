f1ab8e35cebc3b35306142048bfad8d4105168c6
DJ <147062692+KillerDJLang@users.noreply.github.com>
2023-12-13 06:40:14 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Configuration;
+using UnityEngine;
+
+namespace Power
+{
+    [BepInPlugin("DJ.UnlimitedPower", "DJs Unlimited Power", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static GameObject Hook;
+        internal static UnlimitedPower Script;
+        internal static ManualLogSource logger;
+        internal static ConfigEntry<bool> EnablePowerChanges;
+
+        void Awake()
+        {
+            logger = Logger;
+            Logger.LogInfo("Loading Unlimited...Powaaaaaa");
+            Hook = new GameObject("Power Object");
+            Script = Hook.AddComponent<UnlimitedPower>();
+            DontDestroyOnLoad(Hook);
+
+            EnablePowerChanges = Config.Bind(
+                "Power",
+                "Enable Dynamic Power Changes",
+                true,
+                new ConfigDescription("If enabled, allows power switches to be dynamically turned on at random points throughout your raids.",
+                null,
+                new ConfigurationManagerAttributes { IsAdvanced = false, ShowRangeAsPercent = false, Order = 1 }));
+        }
+    }
+}
---
+++ UnlimitedPower.cs
@@ -0,0 +1,50 @@
+﻿using EFT;
+using UnityEngine;
+using Comfort.Common;
+using EFT.Interactive;
+using System.Reflection;
+
+namespace Power
+{
+    public class UnlimitedPower : MonoBehaviour
+    {
+        float timer;
+        float PowerTimer;
+        float PowerOnTimeRange = Random.Range(60f, 180f);
+
+        Player player
+        { get => gameWorld.MainPlayer; }
+
+        GameWorld gameWorld
+        { get => Singleton<GameWorld>.Instance; }
+
+        void Update()
+        {
+            if (!Ready())
+            {
+                timer = 0f;
+                PowerTimer = 0f;
+                return;
+            }
+
+            timer += Time.deltaTime;
+            if (Plugin.EnablePowerChanges.Value) PowerTimer += Time.deltaTime;
+
+            if (PowerTimer >= PowerOnTimeRange)
+            {
+                PowerOn();
+                PowerTimer = 0f;
+                PowerOnTimeRange = Random.Range(900f, 1800f);
+            }
+        }
+
+        void PowerOn()
+        {
+            foreach (Switch pSwitch in FindObjectsOfType<Switch>())
+            {
+                typeof(Switch).GetMethod("Open", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(pSwitch, null);
+            }
+        }
+        public bool Ready() => gameWorld != null && gameWorld.AllAlivePlayersList != null && gameWorld.AllAlivePlayersList.Count > 0 && !(player is HideoutPlayer);
+    }
+}
